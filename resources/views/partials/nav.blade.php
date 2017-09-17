c4a579594e7fa30a9b7ec1214b125466ea7df034
dessHub <deskip95@gmail.com>
2017-09-17 14:16:32 +0300

    banner

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,30 +3,12 @@
   <i class="iconic iconic-close" id="mobileNav2"></i>
 </div>
 <ul>
-  <li class="menu-item-has-children"><a href="#">Home</a>
-    <ul class="sub-menu">
-      <li><a href="index.html">Default Layout</a></li>
-      <li><a href="home-2.html">Home v2</a></li>
-      <li><a href="home-3.html">Home v3</a></li>
-      <li><a href="home-4.html">Home v4</a></li>
-    </ul>
-  </li>
-  <li class="menu-item-has-children"><a href="#">Properties</a>
+  <li class="menu-item-has-children"><a href="{{ url('/') }}">Home</a></li>
+  <li class="menu-item-has-children"><a href="{{ url('/properties') }}">Homes</a>
   <ul class="sub-menu">
-    <li><a href="property-list.html">Property List</a>
-    <ul class="sub-menu">
-      <li><a href="property-list-sidebar-left.html">Sidebar Left</a></li>
-      <li><a href="property-list-sidebar-right.html">Sidebar Right</a></li>
-    </ul>
-    </li>
-    <li><a href="property-details.html">Property Details</a></li>
+    <li><a href="property-list.html">Homes For Sale</a></li>
+    <li><a href="property-details.html">Homes For Rent</a></li>
   </ul>
-  </li>
-  <li class="menu-item-has-children"><a href="#">Pages</a>
-  <ul class="sub-menu">
-        <li><a href="page-layout-left-sidebar.html">Sidebar Left</a></li>
-        <li><a href="page-layout-right-sidebar.html">Sidebar Right</a></li>
-      </ul>
   </li>
   <li class="menu-item-has-children"><a href="#">Blog</a>
     <ul class="sub-menu">
@@ -47,38 +29,6 @@
   <li><a href="agents.html">Agents</a>
   <ul>
     <li><a href="agents-details.html">Agent Details</a></li>
-  </ul>
-  </li>
-  <li class="menu-item-has-children"><a href="#">ShortCodes</a>
-  <ul class="sub-menu">
-    <li><a href="#">Typography And Components</a>
-      <ul class="dir-right">
-        <li><a href="page-typography.html">General Typography</a></li>
-        <li><a href="page-heading.html">Heading</a></li>
-        <li><a href="page-blockquote.html">Blockquote</a></li>
-        <li><a href="page-boxshadow.html">Box Shadow</a></li>
-        <li><a href="page-testimonials.html">Testimonials</a></li>
-        <li><a href="page-tagline.html">Tagline Boxes</a></li>
-        <li><a href="page-grid-layout.html">Grid Layout</a></li>
-      </ul>
-    </li>
-    <li><a href="page-buttons.html">Buttons UI</a>
-    </li>
-    <li><a href="page-icons.html">Icons</a>
-    </li>
-  </ul>
-  </li>
-  <li class="menu-item-has-children"><a href="#">Misc</a>
-  <ul class="dir-right level-1 sub-menu">
-    <li><a href="#">Header</a>
-    <ul class="dir-right">
-      <li><a href="header-v1.html">Header v1</a></li>
-       <li><a href="header-v2.html">Header v2</a></li>
-       <li><a href="banner-slider.html">Header Slider v1</a></li>
-       <li><a href="banner-slider-with-caption.html">Header Slider v2</a></li>
-    </ul>
-    </li>
-    <li><a href="page-footer-v1.html">Footer</a></li>
   </ul>
   </li>
 </ul>
@@ -97,310 +47,10 @@
 
 </div>
 <div class="sidebar-full-height">
-  <div class="sidebar-close-bar">
-    <a href="#" class="iconic iconic-close2"></a>
-  </div>
-  <div class="sidebar-close-bar2">
-    <a href="#" class="iconic iconic-close2"></a>
-  </div>
-  <!-- widget -->
-  <div class="widget-1 widget-1-address" id="widget-1-num-1">
-    <h3>your dream  <br>home <br>
-      search <br>
-      ends here. </h3>
-      <div class="logo-image">
-      <img src="images/logo.png" height="225" width="130" alt="">
-    </div>
-      <p>Contact us: <br>
-      Head Office <br>
-      STEP IIT KHARAGPUR <br>
-      WEST BENGAL – 721302 </p>
-  </div>
-   <!-- widget // -->
-   <!-- widget -->
-  <div class="widget-1 widget-1-search" id="widget-1-num-2">
-    <h4>Search your property</h4>
-    <div class="form-tab pad-top-small">
-        <a class="active"  href="#widget-1-form-1">All</a>
-        <a href="#widget-1-form-2">Sell</a>
-        <a href="#widget-1-form-3">Rent</a>
-    </div>
-    <div class="search-form-2 map-search-control form-cotent-map">
-      <form class="form-1" id="widget-1-form-1">
-        <div class="row pad-top-large">
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Property types</option>
-            </select>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Select city</option>
-            </select>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Bedrooms</option>
-            </select>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Bathrooms</option>
-            </select>
-          </div>
-          <div class="col-sm-12">
-            <p>Select your price range</p>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Mini</option>
-            </select>
-          </div>
-          <div class="col-xs-12 form-group">
-            <div  class="no-slider">
-            <span class="info">
-            <span class="min">Minimum</span>
-            <span class="max pull-right">Max</span>
-            </span>
-            </div>
-            <div id="slider-tooltip1" class="slider-tooltip">
-            </div>
-          </div>
-          <div class="col-xs-6">
-            <a href="#" class="adv-search"><u>Advance Search Option</u></a>
-          </div>
-          <div class="col-xs-6">
-            <a href="#" class="btn btn-dark pull-right hvr-shutter-out-vertical">Find property</a>
-          </div>
-        </div>
-      </form>
-      <form class="form-1 hidden" id="widget-1-form-2">
-        <div class="row pad-top-large">
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Property types</option>
-            </select>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Select city</option>
-            </select>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Bedrooms</option>
-            </select>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Bathrooms</option>
-            </select>
-          </div>
-          <div class="col-sm-12">
-            <p>Select your price range</p>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Mini</option>
-            </select>
-          </div>
-          <div class="col-xs-12 form-group">
-            <div  class="no-slider">
-            <span class="info">
-            <span class="min">Minimum</span>
-            <span class="max pull-right">Max</span>
-            </span>
-            </div>
-            <div id="slider-tooltip2" class="slider-tooltip">
-            </div>
-          </div>
-          <div class="col-xs-6">
-            <a href="#" class="adv-search"><u>Advance Search Option</u></a>
-          </div>
-          <div class="col-xs-6">
-            <a href="#" class="btn btn-dark pull-right hvr-shutter-in-vertical">Find property</a>
-          </div>
-        </div>
-      </form>
-      <form class="form-1 hidden" id="widget-1-form-3">
-        <div class="row pad-top-large">
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Property types</option>
-            </select>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Select city</option>
-            </select>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Bedrooms</option>
-            </select>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Bathrooms</option>
-            </select>
-          </div>
-          <div class="col-sm-12">
-            <p>Select your price range</p>
-          </div>
-          <div class="col-sm-6 form-group">
-            <select class="form-control">
-              <option>Mini</option>
-            </select>
-          </div>
-          <div class="col-xs-12 form-group">
-            <div  class="no-slider">
-            <span class="info">
-            <span class="min">Minimum</span>
-            <span class="max pull-right">Max</span>
-            </span>
-            </div>
-            <div id="slider-tooltip3" class="slider-tooltip">
-            </div>
-          </div>
-          <div class="col-xs-6">
-            <a href="#" class="adv-search"><u>Advance Search Option</u></a>
-          </div>
-          <div class="col-xs-6">
-            <a href="#" class="btn btn-dark pull-right hvr-bounce-to-bottom">Find property</a>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-  <!-- widget // -->
-  <!-- widget -->
-  <div class="widget-1" id="widget-1-num-3">
-    <h3 class="large-heading">Search your
-    dream homes</h3>
-    <p class="large-text-1">in your city
-    just click on the availabe icons</p>
-    <p><img src="images/icons/map-marker2.png" height="63" width="44" alt=""></p>
-    <div class="pad-top-small row-float">
-      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-    </div>
-  </div>
-  <!-- widget // -->
-  <!-- widget -->
-  <div class="widget-1" id="widget-1-num-4">
-    <h3 class="large-heading">your dream <br>
-      homes</h3>
-    <span class="iconic iconic-search"></span>
-    <p class="large-text-1">Your search result listed here</p>
-    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-  </div>
-  <!-- widget // -->
-  <!-- widget -->
-   <div class="widget-1 widget-1-rel-prop" id="widget-1-num-5">
-     <h3 class="medium-heading">related property
-      you may like</h3>
-        <div class="rltd-property">
-          <ul>
-            <li>
-              <a href="#">
-                <img src="images/related-property/image-1.jpg" height="106" width="165" alt="">
-                <span class="overflow">
-                  <span class="type">SALE</span>
-                </span>
-              </a>
-              <div class="description">
-                <p>Appertment: <br>
-                5111 140th<br>
-                Ave NE Bellevue,<br>
-                WA 98005 <br>
-                <span class="price">$2100</span></p>
-              </div>
-            </li>
-            <li>
-              <a href="#">
-                <img src="images/related-property/image-2.jpg" height="106" width="165" alt="">
-                <span class="overflow">
-                  <span class="type">RENT</span>
-                </span>
-              </a>
-              <div class="description">
-                <p>Appertment: <br>
-                5111 140th<br>
-                Ave NE Bellevue,<br>
-                WA 98005 <br>
-                <span class="price">$2100</span></p>
-              </div>
-            </li>
-            <li>
-              <a href="#">
-                <img src="images/related-property/image-3.jpg" height="106" width="165" alt="">
-                 <span class="overflow">
-                  <span class="type">RENT</span>
-                </span>
-              </a>
-              <div class="description">
-                <p>Appertment: <br>
-                5111 140th<br>
-                Ave NE Bellevue,<br>
-                WA 98005 <br>
-                <span class="price">$2100</span></p>
-              </div>
-            </li>
-            <li>
-              <a href="#">
-                <img src="images/related-property/image-4.jpg" height="106" width="165" alt="">
-                 <span class="overflow">
-                  <span class="type">RENT</span>
-                </span>
-              </a>
-              <div class="description">
-                <p>Appertment: <br>
-                5111 140th<br>
-                Ave NE Bellevue,<br>
-                WA 98005 <br>
-                <span class="price">$2100</span></p>
-              </div>
-            </li>
 
-          </ul>
-        </div>
-   </div>
+
   <!-- widget // -->
-  <!-- widget  -->
-  <div class="widget-1" id="widget-1-num-6">
-    <h3 class="large-heading">Something <br>
-    about us!</h3>
-    <span class="iconic iconic-info pink"></span>
-    <p class="large-text-1">know more about us before commit</p>
-    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-  </div>
-  <!-- widget // -->
-  <!-- widget  -->
-  <div class="widget-1" id="widget-1-num-7">
-    <h3 class="large-heading">know your <br>
-    dream home!</h3>
-    <span class="iconic iconic-user style-1"></span>
-    <p class="large-text-1">know more about us before commit</p>
-    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-  </div>
-  <!-- widget // -->
-  <!-- widget  -->
-  <div class="widget-1" id="widget-1-num-8">
-    <h3 class="large-heading">find us <br>
-    anywhere!</h3>
-    <span class="iconic iconic-search style-1"></span>
-    <p class="large-text-1">know more about us before commit</p>
-    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-  </div>
-  <!-- widget // -->
-   <!-- widget  -->
-  <div class="widget-1" id="widget-1-num-9">
-    <h3 class="large-heading">Our <br>
-    agents!</h3>
-    <span class="iconic iconic-v-card pink"></span>
-    <p class="large-text-1">know more about us before commit</p>
-    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-  </div>
-  <!-- widget // -->
+
   <div class="sidebar-close-bar visible-xs-block">
     <a href="#" class="iconic iconic-close2"></a>
   </div>
@@ -419,23 +69,11 @@
         <i class="iconic iconic-menu"></i>
       </a>
        <ul>
-  <li class="menu-item-has-children"><a href="#">Home</a>
-    <ul class="sub-menu">
-      <li><a href="index.html">Default Layout</a></li>
-      <li><a href="home-2.html">Home v2</a></li>
-      <li><a href="home-3.html">Home v3</a></li>
-      <li><a href="home-4.html">Home v4</a></li>
-    </ul>
-  </li>
-  <li class="menu-item-has-children"><a href="#">Properties</a>
+  <li class="menu-item-has-children"><a href="{{ url('/')}}">Home</a></li>
+  <li class="menu-item-has-children"><a href="{{ url('/properties')}}">Properties</a>
   <ul class="sub-menu">
-    <li><a href="property-list.html">Property List</a>
-    <ul class="sub-menu">
-      <li><a href="property-list-sidebar-left.html">Sidebar Left</a></li>
-      <li><a href="property-list-sidebar-right.html">Sidebar Right</a></li>
-    </ul>
-    </li>
-    <li><a href="property-details.html">Property Details</a></li>
+    <li><a href="property-list.html">Homes For Sale</a></li>
+    <li><a href="property-details.html">Homes For Rent</a></li>
   </ul>
   </li>
   <li class="menu-item-has-children"><a href="#">Pages</a>
@@ -465,58 +103,10 @@
     <li><a href="agents-details.html">Agent Details</a></li>
   </ul>
   </li>
-  <li class="menu-item-has-children"><a href="#">ShortCodes</a>
-  <ul class="sub-menu">
-    <li><a href="#">Typography And Components</a>
-      <ul class="dir-right">
-        <li><a href="page-typography.html">General Typography</a></li>
-        <li><a href="page-heading.html">Heading</a></li>
-        <li><a href="page-blockquote.html">Blockquote</a></li>
-        <li><a href="page-boxshadow.html">Box Shadow</a></li>
-        <li><a href="page-testimonials.html">Testimonials</a></li>
-        <li><a href="page-tagline.html">Tagline Boxes</a></li>
-        <li><a href="page-grid-layout.html">Grid Layout</a></li>
-      </ul>
-    </li>
-    <li><a href="page-buttons.html">Buttons UI</a>
-    </li>
-    <li><a href="page-icons.html">Icons</a>
-    </li>
-  </ul>
-  </li>
-  <li class="menu-item-has-children"><a href="#">Misc</a>
-  <ul class="dir-right level-1 sub-menu">
-    <li><a href="#">Header</a>
-    <ul class="dir-right">
-      <li><a href="header-v1.html">Header v1</a></li>
-       <li><a href="header-v2.html">Header v2</a></li>
-       <li><a href="banner-slider.html">Header Slider v1</a></li>
-       <li><a href="banner-slider-with-caption.html">Header Slider v2</a></li>
-    </ul>
-    </li>
-    <li><a href="page-footer-v1.html">Footer</a></li>
-  </ul>
-  </li>
 </ul>
 
     </div>
-     <div class="info wow bounceInUp animated" data-wow-delay="1s">
-  <span class="type"><span class="text">Rent</span></span>
-    <div class="description">
-      <p class="title">1102 5th St Kirkland,
-        WA 98033
-      </p>
-      <p class="meta"><span class="meta-block">Bed : 3</span>
-      <span class="meta-block">Parking : 1 </span>
-      <span class="meta-block">Bath : 2</span>
-      <span class="meta-block">1500 sq ft</span>
-      </p>
-    </div>
-    <div class="price">
-      <p class="rate">$3500</p>
-      <p><a href="#" class="btn btn-default btn-sm hvr-shutter-out-vertical">View property</a></p>
-    </div>
-  </div>
+
   </div>
 
   </div>
